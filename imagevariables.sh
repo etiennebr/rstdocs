348e3dcd59049ef1958bc7cc1f2f3696d5b47b7b
Rashad M <rashad.kanavath@c-s.fr>
2014-12-16 11:00:41 +0100

    unused script

--- imagevariables.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for rstfile in "Monteverdi.rst" "stereo.rst" "featextract.rst" ; do 
-    perl -pi -e 's/\|image\|\ image::/"image".++$i/ge' $rstfile
-    perl -pi -e 's/\|image\|/"image".++$i/ge' $rstfile
-    perl -pi -e 's|\.\.\/Art|"./Art"|ge' $rstfile
-    perl -pi -e 's|`__|"`_"|ge' $rstfile
-done
